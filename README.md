5b3b8e7d480c6d62ecbef190360be166f57dda36
AndenPMS <Philipp_Schuster@hotmail.de>
2025-12-11 21:03:02 -0300

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# alura-challenge-conversor
+# Conversor de Monedas Java
+
+## 📜 Descripción del Proyecto
+
+Este es una aplicación de **consola** desarrollada en **Java** para la conversión de divisas. El proyecto funciona mediante el consumo de una API externa (**ExchangeRate-API**) para obtener las tasas de cambio **en tiempo real**.
+
+Las divisas soportadas en el menú interactivo incluyen: **USD**, **Peso Argentino**, **Real Brasileño**, **Peso Colombiano** y **Euro**.
+## 📜 Cómo Usar
+
+    ●Abre el proyecto en tu IDE (como IntelliJ IDEA).
+
+    ●Verifica que la dependencia Gson se haya cargado correctamente.
+
+    ●Ejecuta el método main de la clase principal: com.alura.conversor.Menu.
+
+Una vez iniciado, el programa presentará el menú de 10 opciones de conversión. Simplemente ingresa el número de la opción deseada y la cantidad a convertir.
+
+
+## ⚙️ Requisitos Clave
+
+Para compilar y ejecutar el proyecto, asegúrate de tener:
+
+1.  **Java Development Kit (JDK) 17+**.
+2.  La librería **Google Gson**, esencial para el procesamiento de los datos JSON de la API.
+
+Si estás utilizando **Maven** para gestionar dependencias, incluye el siguiente bloque en tu `pom.xml`:
+
+
+```xml
+<dependency>
+    <groupId>com.google.code.gson</groupId>
+    <artifactId>gson</artifactId>
+    <version>2.10.1</version>
+</dependency>
+```
--- src/com/alura/conversor/Conversor.java
+++ src/com/alura/conversor/Conversor.java
@@ -1,51 +1,51 @@
-package com.alura.conversor;
+    package com.alura.conversor;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.text.DecimalFormat;
+    import java.util.InputMismatchException;
+    import java.util.Scanner;
+    import java.text.DecimalFormat;
 
-public class Conversor {
+    public class Conversor {
 
-    private String moneda;
-    private String moneda2;
-    private int codigo;
-    //static para garantizar que db no sea sobreescrito, aun al crear multiples objetos db o reinicializar db.
-    private static DataBase db = new DataBase();
+        private String moneda;
+        private String moneda2;
+        private int codigo;
+        //static para garantizar que db no sea sobreescrito, aun al crear multiples objetos db o reinicializar db.
+        private static DataBase db = new DataBase();
 
-    public Conversor(int codigo, String moneda, String moneda2){
-        this.moneda = moneda;
-        this.moneda2 = moneda2;
-        this.codigo = codigo;
-    }
+        public Conversor(int codigo, String moneda, String moneda2){
+            this.moneda = moneda;
+            this.moneda2 = moneda2;
+            this.codigo = codigo;
+        }
 
-    private void mostrarValor(float val){
-        DecimalFormat valueFormat = new DecimalFormat("0.00000000");
-        System.out.println("1"+this.moneda+" = "+valueFormat.format(val)+this.moneda2);
-    }
+        private void mostrarValor(float val){
+            DecimalFormat valueFormat = new DecimalFormat("0.00000000");
+            System.out.println("1"+this.moneda+" = "+valueFormat.format(val)+this.moneda2);
+        }
 
-    public void convertir() {
-        Scanner input = new Scanner(System.in);
-        float val = db.consultarValor(this.codigo, this.moneda, this.moneda2);
+        public void convertir() {
+            Scanner input = new Scanner(System.in);
+            float val = db.consultarValor(this.codigo, this.moneda, this.moneda2);
 
-        mostrarValor(val);
+            mostrarValor(val);
 
-        int cantidad;
-        do {
-            cantidad = -1;
-            try{
-                System.out.println("Ingrese cantidad deseada a convertir: ");
-                cantidad = input.nextInt();
-                if(cantidad<0){
-                    System.out.println("ERROR: Ingrese una cantidad positiva");
+            int cantidad;
+            do {
+                cantidad = -1;
+                try{
+                    System.out.println("Ingrese cantidad deseada a convertir: ");
+                    cantidad = input.nextInt();
+                    if(cantidad<0){
+                        System.out.println("ERROR: Ingrese una cantidad positiva");
+                    }
+                }catch (InputMismatchException e){
+                    input.next();
+                    System.out.println("ERROR: Ingrese un numero");
+                    continue;
                 }
-            }catch (InputMismatchException e){
-                input.next();
-                System.out.println("ERROR: Ingrese un numero");
-                continue;
-            }
 
-        }while(cantidad<0);
-        DecimalFormat moneyFormat = new DecimalFormat("0.00");
-        System.out.println(cantidad+" "+this.moneda+" = "+moneyFormat.format(cantidad*val)+" "+this.moneda2);
+            }while(cantidad<0);
+            DecimalFormat moneyFormat = new DecimalFormat("0.00");
+            System.out.println(cantidad+" "+this.moneda+" = "+moneyFormat.format(cantidad*val)+" "+this.moneda2);
+        }
     }
-}
